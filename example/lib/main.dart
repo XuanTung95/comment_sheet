51006d1af94f42fd9e52fb41e99d986d9cc749bb
tungpx <findyou795@gmail.com>
2022-12-21 00:59:04 +0700

    update ui

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,28 +1,19 @@
 import 'package:comment_sheet/comment_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -91,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
                                 if (info.currentTop < 500) {
                                   return 200;
                                 }
-
                                 return 600;
                               },
                               scrollController: scrollController,
@@ -138,49 +119,153 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Stack buildGrabbing(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topRight,
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10), topRight: Radius.circular(10))),
-          height: 100,
+  Widget buildGrabbing(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: const BoxDecoration(
+        color: Color(0xFF0F0F0F),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(12),
+          topRight: Radius.circular(12),
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              icon: Icon(Icons.close)),
-        ),
-      ],
+      ),
+      padding: EdgeInsets.only(top: 10, bottom: 2),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 40,
+            height: 4,
+            margin: EdgeInsets.only(top: 4),
+            decoration: BoxDecoration(color: Colors.white60, borderRadius: BorderRadius.circular(100)),
+          ),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    "Bình luận",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6.0),
+                  child: Text(
+                    "48",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white70,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.menu_sharp,
+                  size: 26,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      size: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Color(0xFF292929),
+            width: double.infinity,
+            height: 1,
+          ),
+        ],
+      ),
     );
   }
 
-  SliverList buildSliverList() {
-    return SliverList(
-        delegate: SliverChildBuilderDelegate((context, index) {
-      return Material(
-        color: Colors.amberAccent.shade100,
-        child: InkWell(
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-          child: Container(
-            height: 100,
-            // color: Colors.amberAccent.shade100,
-            width: double.infinity,
-            child: Center(
-              child: Text("Item: $index"),
-            ),
+  Widget buildSliverList() {
+    return SliverClip(
+      child: SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+        return ListItemWidget();
+      }, childCount: 10)),
+    );
+  }
+}
+
+class ListItemWidget extends StatelessWidget {
+  const ListItemWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Container(
+          color: Color(0xFF0F0F0F),
+          width: double.infinity,
+          padding: const EdgeInsets.only(top: 12, bottom: 0, left: 10, right: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 5, right: 10),
+                child: ClipOval(
+                  child: Image.network(
+                    "https://yt3.ggpht.com/yti/AJo0G0kUnHqoybmWPJG4GNm0G-lfCiCPbEP62v5tq9PZsA=s48-c-k-c0x00ffffff-no-rj",
+                    width: 25,
+                    height: 25,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Andrea Quintanilla * 3 tháng trước',
+                      style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400, color: Color(0xFFAEAEAE)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 6, bottom: 12),
+                      child: Text(
+                        'Que buen trabajo, que buenos enganches, genial!!!!  MTV la tenes adentro, jajaja. Saludos cordiales desde Buenos Aires, Argentina, Argentina, Argentina!',
+                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, color: Color(0xFFF6F6F6)),
+                      ),
+                    ),
+                    Row(children: [
+                      Icon(Icons.thumb_up_outlined, size: 15, color: Colors.white,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                        child: Icon(Icons.thumb_down_alt_outlined, size: 15, color: Colors.white,),
+                      ),
+                      Icon(Icons.comment_outlined, size: 15, color: Colors.white,),
+                    ],)
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-      );
-    }, childCount: 10));
+      ),
+    );
   }
 }
